e841ce6ad71e44848ded748208a5262730109fa1
VeselinDenchev <den4ev01@gmail.com>
2022-06-14 13:20:20 +0300

    Add comments to AccountTypeLimitConstant class

--- BankProject/Constants/AccountTypeLimitConstant.cs
+++ BankProject/Constants/AccountTypeLimitConstant.cs
@@ -2,18 +2,25 @@
 {
     public static class AccountTypeLimitConstant
     {
+        // REGULAR ACCOUNT TYPE
         public const decimal REGULAR_ACCOUNT_TYPE_LOWER_LIMIT = 0;
 
         public const decimal REGULAR_ACCOUNT_TYPE_UPPER_LIMIT = 100_000;
 
+
+        // BRONZE ACCOUNT TYPE
         public const decimal BRONZE_ACCOUNT_TYPE_LOWER_LIMIT = REGULAR_ACCOUNT_TYPE_UPPER_LIMIT;
 
         public const decimal BRONZE_ACCOUNT_TYPE_UPPER_LIMIT = 200_000;
 
+
+        // GOLD ACCOUNT TYPE
         public const decimal GOLD_ACCOUNT_TYPE_LOWER_LIMIT = BRONZE_ACCOUNT_TYPE_UPPER_LIMIT;
 
         public const decimal GOLD_ACCOUNT_TYPE_UPPER_LIMIT = 500_000;
 
+
+        // PLATINUM ACCOUNT TYPE
         public const decimal PLATINUM_ACCOUNT_TYPE_LOWER_LIMIT = GOLD_ACCOUNT_TYPE_UPPER_LIMIT;
 
         public const decimal PLATINUM_ACCOUNT_TYPE_UPPER_LIMIT = decimal.MaxValue;
