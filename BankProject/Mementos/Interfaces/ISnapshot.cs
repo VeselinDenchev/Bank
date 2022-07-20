8f24239b0390cea006fc6e0b705f4c0b02775b5a
VeselinDenchev <den4ev01@gmail.com>
2022-07-20 11:32:42 +0300

    Create ISnapshot interface
    Move CreateSnapshot() and ReverseSnapshot() methods from IBank to ISnapshot. Implement ISnapshot in Bank class.

---
+++ BankProject/Mementos/Interfaces/ISnapshot.cs
@@ -0,0 +1,9 @@
+﻿namespace BankProject.Mementos.Interfaces
+{
+    public interface ISnapshot
+    {
+        public void CreateSnapshot();
+
+        public void RevertSnapshot(out bool isSuccessful);
+    }
+}
